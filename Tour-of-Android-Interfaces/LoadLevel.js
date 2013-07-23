69598e0a7ca777b8479701b3221180e4cc759f64
gskielian <greg@techlabeducation.com>
2013-07-22 19:03:33 -0700

    Update LoadLevel.js

--- Tour-of-Android-Interfaces/LoadLevel.js
+++ Tour-of-Android-Interfaces/LoadLevel.js
@@ -1,6 +1,9 @@
 #pragma strict
+
+var levelNumber : int = 1;
+
      
 function OnTriggerEnter (col : Collider)
       {
-          Application.LoadLevel(1); //loads level 1 when some object collides with this object's box-collider (added via the physics sub-menu)
+          Application.LoadLevel(levelNumber); //loads level 1 when some object collides with this object's box-collider (added via the physics sub-menu)
       }
